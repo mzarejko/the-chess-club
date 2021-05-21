2e0121be0ce5638c4319ad60deadf63121d90319
vostro <haker@vostro.home>
2021-05-21 14:18:35 +0200

    make it better

--- backend/base/development.py
+++ backend/base/development.py
@@ -60,6 +60,8 @@ STATICFILES_DIRS = [
 ]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_URL = '/static/'
+STATIC_MEDIA = '/media/'
 
 # ASGI is for djnago channel
 ASGI_APPLICATION = 'base.routing.application'
